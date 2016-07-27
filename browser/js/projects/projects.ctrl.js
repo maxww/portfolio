887b08a802a4b50ca8abaa5785a947256dc899fd
Taffy Chen <tornglinchen@gmail.com>
2016-07-27 16:43:08 -0400

    made single project view

--- browser/js/projects/projects.ctrl.js
+++ browser/js/projects/projects.ctrl.js
@@ -1,5 +1,9 @@
 app.controller("projectsCtrl", function ($scope, projects) {
-	$scope.projects = projects;
-	$scope.pathparadoxImgs = projects[0].images;
-	$scope.moreCatImgs = projects[1].images;
-})
+		$scope.projects = projects;
+		$scope.pathparadoxImgs = projects[0].images;
+		$scope.moreCatImgs = projects[1].images;
+	})
+	.controller("projectCtrl", function ($scope, projects, $stateParams) {
+		if ($stateParams.name === "pathparadox") $scope.project = projects[0];
+		if ($stateParams.name === "morecat") $scope.project = projects[1];
+	})
--- browser/js/projects/projects.state.js
+++ browser/js/projects/projects.state.js
@@ -1,12 +1,23 @@
 app.config(function ($stateProvider) {
-	$stateProvider.state("projects", {
-		url: "/projects",
-		templateUrl: "/js/projects/projects.html",
-		controller: "projectsCtrl",
-		resolve: {
-			projects: function (projectsFty) {
-				return projectsFty.getProjects();
+	$stateProvider
+		.state("projects", {
+			url: "/projects",
+			templateUrl: "/js/projects/projects.html",
+			controller: "projectsCtrl",
+			resolve: {
+				projects: function (projectsFty) {
+					return projectsFty.getProjects();
+				}
 			}
-		}
-	})
+		})
+		.state("project", {
+			url: "/projects/:name",
+			templateUrl: "/js/projects/project.html",
+			controller: "projectCtrl",
+			resolve: {
+				projects: function (projectsFty) {
+					return projectsFty.getProjects();
+				}
+			}
+		})
 })
