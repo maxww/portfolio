6d8de355a84f96c7e09e56657880022907d4024e
Taffy Chen <tornglinchen@gmail.com>
2016-07-27 15:55:16 -0400

    rearranged project view

--- browser/js/projects/projects.ctrl.js
+++ browser/js/projects/projects.ctrl.js
@@ -1,3 +1,5 @@
 app.controller("projectsCtrl", function ($scope, projects) {
 	$scope.projects = projects;
+	$scope.pathparadoxImgs = projects[0].images;
+	$scope.moreCatImgs = projects[1].images;
 })
