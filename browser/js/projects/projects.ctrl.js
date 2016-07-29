7b2e086b2b4e95d2ef7f9573ca8caeeb3ff818d6
Taffy Chen <tornglinchen@gmail.com>
2016-07-29 13:00:46 -0400

    finished project page

--- browser/js/projects/projects.ctrl.js
+++ browser/js/projects/projects.ctrl.js
@@ -4,6 +4,12 @@ app.controller("projectsCtrl", function ($scope, projects) {
 		$scope.moreCatImgs = projects[1].images;
 	})
 	.controller("projectCtrl", function ($scope, projects, $stateParams) {
-		if ($stateParams.name === "pathparadox") $scope.project = projects[0];
-		if ($stateParams.name === "morecat") $scope.project = projects[1];
+		if ($stateParams.name === "pathparadox") {
+			$scope.project = projects[0];
+			$scope.pathparadox = true;
+		}
+		if ($stateParams.name === "morecat") {
+			$scope.project = projects[1];
+			$scope.morecat = true;
+		}
 	})
